4846c60c397a5c7723e876343f364727addef2cb
lleiding <lleiding@users.noreply.github.com>
2019-11-02 16:15:23 +0100

    Create secretsanta.jl

---
+++ secretsanta.jl
@@ -0,0 +1,15 @@
+using Random
+
+names = ["John",
+"James",
+"Kent",
+"Judy"]
+
+pairs = [1, 1, 2, 2]
+
+santaindex = shuffle(eachindex(names))
+while any(pairs.== pairs[wichtelidx])
+    santaindex = shuffle(eachindex(names))
+    end
+
+secretsanta = names[wichtelidx]
