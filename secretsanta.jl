726657e23fb78f9a096f8e87dc18a64be6712c49
skogtroll-ludvik <l.leidinger@gmx.net>
2019-11-03 16:30:03 +0100

    add mailing functionality via msmtp

--- secretsanta.jl
+++ secretsanta.jl
@@ -11,7 +11,7 @@ Order of columns is: names, pairs, e-mail addresses.
 """
 function readtable(tablefile)
     if isfile(tablefile)
-        table = CSV.read(tablefile)
+        table = CSV.read(tablefile, comment="#")
     else
         error("$tablefile is not a valid file.")
     end
@@ -32,6 +32,38 @@ function getsecretsanta(names, pairs)
     secretsanta = names[santaindex]
 end
 
-table = readtable(ARGS[1])
+"""
+    mailsecretsanta(t)
 
-table.Santa = getsecretsanta(table[:,1], table[:,2])
+Notifies participants via e-mail using information in table t.
+"""
+function mailsecretsanta(table)
+    if length(size(table)) > 1
+        for i in 1:size(table, 1)
+            name = table.Name[i]
+            mail = table.Address[i]
+            santa = table.Santa[i]
+            message = "To: $mail\r\nSubject: Weihnachtswichteln 2019\r\n\r\nHallo $name,\r\n\r\nDu darfst heuer $santa bewichteln.\r\n\r\nLG\r\nDein Weihnachtswichtelskript\r\n"
+            print(pipeline(`echo $message`, `msmtp -a gmx $mail`))
+        end
+    else
+        name = table.Name
+        mail = table.Address
+        santa = table.Santa
+        message = "To: $mail\r\nSubject: Weihnachtswichteln 2019\r\n\r\nHallo $name,\r\n\r\nDu darfst heuer $santa bewichteln.\r\n\r\nLG\r\nDein Weihnachtswichtelskript\r\n"
+        run(pipeline(`echo $message`, `msmtp -a gmx $mail`))
+    end        
+end
+
+"""
+    runsecretsanta()
+
+Runs the secret santa script.
+"""
+function runsecretsanta(filename)
+    table = readtable(filename)
+    table.Santa = getsecretsanta(table[:,1], table[:,2])
+    mailsecretsanta(table)
+end
+
+runsecretsanta(ARGS[1])
