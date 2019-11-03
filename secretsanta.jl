086742b6016f010ab477db05b1ab3f2fc49e43aa
skogtroll-ludvik <l.leidinger@gmx.net>
2019-11-03 14:13:52 +0100

    shebang, use CSV, factorisation, documentation

--- secretsanta.jl
+++ secretsanta.jl
@@ -1,15 +1,37 @@
+#!/usr/bin/env julia
+
 using Random
+using CSV
 
-names = ["John",
-"James",
-"Kent",
-"Judy"]
+"""
+    readtable(f)
 
-pairs = [1, 1, 2, 2]
-
-santaindex = shuffle(eachindex(names))
-while any(pairs.== pairs[wichtelidx])
-    santaindex = shuffle(eachindex(names))
+Reads a csv file f containing information of secret santa participants.
+Order of columns is: names, pairs, e-mail addresses.
+"""
+function readtable(tablefile)
+    if isfile(tablefile)
+        table = CSV.read(tablefile)
+    else
+        error("$tablefile is not a valid file.")
     end
+    table
+end
 
-secretsanta = names[wichtelidx]
+"""
+    getsecretsanta(n, p)
+
+Takes two vectors with names, n, and corresponding pairings, p,
+and returns a permutated names vector for secret santa.
+"""
+function getsecretsanta(names, pairs)
+    santaindex = shuffle(eachindex(names))
+    while any(pairs.== pairs[santaindex])
+        santaindex = shuffle(eachindex(names))
+    end
+    secretsanta = names[santaindex]
+end
+
+table = readtable(ARGS[1])
+
+table.Santa = getsecretsanta(table[:,1], table[:,2])
